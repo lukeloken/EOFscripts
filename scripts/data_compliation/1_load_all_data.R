b1e2bd340fcaedb38fce8f97ccee1e69e73d2c45
Luke Loken <lukeloken@gmail.com>
2020-01-22 08:49:10 -0600

    loop to identify 'mod' file in each folder and load into R list.

--- scripts/data_compliation/1_load_all_data.R
+++ scripts/data_compliation/1_load_all_data.R
@@ -1,4 +1,31 @@
 
 
 #Loop through all site folders and load edge of field runoff data
+library(drake)
 
+
+print(path_to_data)
+
+
+data_list <-list()
+folders<-list.files(path_to_data)
+
+folder_nu<-1
+for (folder_nu in 1:length(folders)){
+  folder<-folders[folder_nu]
+  
+  cached_files<-list.files(file.path(path_to_data, folder, 'data_cached'))
+  mod_file <- cached_files[grep("mod_dat" , cached_files)]
+  
+  site_name <- paste(unlist(strsplit(mod_file, "_"))[1], unlist(strsplit(mod_file, "_"))[2], sep="_")
+
+  mod_path <- file.path(path_to_data, folder, 'data_cached', mod_file)
+  
+  data_i <- read.csv(file_in(mod_path), stringsAsFactors = F, header=T)
+  
+  data_list[[folder_nu]]<-data_i
+  names(data_list)[[folder_nu]] <- site_name
+  
+}
+
+head(data_list)

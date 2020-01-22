c05d10770102717aab43a25fa235d9e9a3d856ae
Luke Loken <lukeloken@gmail.com>
2020-01-22 08:59:50 -0600

    change data formats

--- scripts/data_compliation/1_load_all_data.R
+++ scripts/data_compliation/1_load_all_data.R
@@ -15,7 +15,7 @@ for (folder_nu in 1:length(folders)){
   folder<-folders[folder_nu]
   
   cached_files<-list.files(file.path(path_to_data, folder, 'data_cached'))
-  mod_file <- cached_files[grep("mod_dat" , cached_files)]
+  mod_file <- cached_files[grepl("mod_dat" , cached_files)]
   
   site_name <- paste(unlist(strsplit(mod_file, "_"))[1], unlist(strsplit(mod_file, "_"))[2], sep="_")
 
@@ -29,3 +29,14 @@ for (folder_nu in 1:length(folders)){
 }
 
 head(data_list)
+
+data_df <- ldply(data_list, data.frame) %>%
+  mutate(storm_start = as.POSIXct(storm_start),
+         storm_end = as.POSIXct(storm_end),
+         rain_startdate = as.POSIXct(rain_startdate),
+         rain_enddate = as.POSIXct(rain_enddate),
+         ant_discharge_date = as.POSIXct(ant_discharge_date))
+
+
+head(data_df)
+str(data_df)

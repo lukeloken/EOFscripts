05ddfc81af01dd41bbccb97d85beed1a27b78605
Luke Loken <lukeloken@gmail.com>
2020-01-22 15:49:36 -0600

    source functions and second script in pipeline

--- scripts/run_files/compilation_run_file.R
+++ scripts/run_files/compilation_run_file.R
@@ -2,6 +2,12 @@
 
 # Work flow for data compilation and cross-site analysis
 
+#load functions
+source('scripts/functions/fxns_data_compilation.R')
+
 #load all surface water data
 #output is a data.frame named "data_df"
 source('scripts/data_compliation/1_load_all_data.R')
+
+
+source('scripts/data_compliation/2_summarize_data.R')

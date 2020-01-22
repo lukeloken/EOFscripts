a6c31ed9971df50d343c9ffa47df30975093b1ad
Luke Loken <lukeloken@gmail.com>
2020-01-22 15:50:00 -0600

    New script to summarize water quality data by storm event. Output will be a summary by site by water year

---
+++ scripts/data_compliation/2_summarize_data.R
@@ -0,0 +1,25 @@
+
+#Loop through all site folders and load edge of field runoff data
+
+#This is where the data live
+print(path_to_data)
+
+if (exists("data_df")){
+  print("Object data_df exists. Great job!!! You may proceed")
+} else {
+  stop ("no data loaded into R environment. Check 1_load_all_data.R script")
+}
+
+
+data_df2 <- data_df %>%
+  mutate(site = as.character(site),
+         storm_middate = storm_start + difftime(storm_end, storm_start, units='secs')/2) %>%
+  mutate(wateryear = getWY (storm_middate)) %>%
+  select(-file_id, -unique_storm_number, -sub_storms, -rain_startdate, -rain_enddate, -storm_start, -storm_end, -sample_end, -sample_start)
+
+data_wateryear_summary <- data_df2 %>%
+  group_by (site, wateryear) %>%
+  select(-storm_middate) %>%
+  summarize_all(mean, na.rm=T)
+
+data_wateryear_summary

e2008b9f690a2ee91e049a3668c5454b146f9a44
Luke Loken <lukeloken@gmail.com>
2020-01-22 11:58:43 -0600

    add comments

--- scripts/run_files/compilation_run_file.R
+++ scripts/run_files/compilation_run_file.R
@@ -2,5 +2,6 @@
 
 # Work flow for data compilation and cross-site analysis
 
-
+#load all surface water data
+#output is a data.frame named "data_df"
 source('scripts/data_compliation/1_load_all_data.R')

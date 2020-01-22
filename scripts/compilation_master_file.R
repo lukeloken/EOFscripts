a041957514cbd0a4695ffbb225a9a4028cfe46a3
Luke Loken <lukeloken@gmail.com>
2020-01-22 11:55:29 -0600

    changed path file to P drive. Add libraries

--- scripts/compilation_master_file.R
+++ scripts/compilation_master_file.R
@@ -2,6 +2,16 @@
 
 # Master file for running all cross-site comparisons
 
-path_to_data <- "M:/NonPoint Evaluation/GLRI Edge-of-field/R-analysis"
+# path_to_data <- "M:/NonPoint Evaluation/GLRI Edge-of-field/R-analysis"
+path_to_data <- "P:/0301"
+
+
+#load libraries
+library(drake)
+library(plyr)
+library(dplyr)
+library(ggplot2)
+library(lubridate)
+
 
 source('scripts/run_files/compilation_run_file.R')

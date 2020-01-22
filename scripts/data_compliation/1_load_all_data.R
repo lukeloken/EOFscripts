029e9966cb635f8e1b55d686240a03e3bdc60ea6
Luke Loken <lukeloken@gmail.com>
2020-01-22 14:52:51 -0600

    completed data compliation. Output is a single data frame with all the site and storm loading data

--- scripts/data_compliation/1_load_all_data.R
+++ scripts/data_compliation/1_load_all_data.R
@@ -16,9 +16,7 @@ state_nu <- 1
 for (state_nu in 1:length(states)){
   state <- states[state_nu]
   
-  #Create empty list for all sites within this state
-  statesites_list <- list()
-  
+
   if (state %in% c('WI')){
     state_tz <- 'America/Chicago'
   } else if (state %in% c('OH', 'NY', 'IN', 'MI')){
@@ -27,9 +25,13 @@ for (state_nu in 1:length(states)){
   
   folders<-list.files(file.path(path_to_data, "analysis", state))
   
+  #Create empty list for all sites within this state
+  statesites_list <- vector(mode="list", length=length(folders))
+  names(statesites_list) <- folders
+  
   #Remove paired and tile folders
-  folders <- folders[which(grepl('pair', folders)==FALSE)]
-  folders <- folders[which(grepl('TL', folders)==FALSE)]
+  # folders <- folders[which(grepl('pair', folders)==FALSE)]
+  # folders <- folders[which(grepl('TL', folders)==FALSE)]
   
   
   folder_nu<-1
@@ -42,43 +44,51 @@ for (state_nu in 1:length(states)){
     rundates <- as.Date(paste0(rundate_folders, "-01"), format="%Y-%m-%d")
     recent_folder <- rundate_folders[which.max(rundates)]
     
-    tables <- list.files(file.path(path_to_data, "analysis", state, folder, "results", recent_folder, "tables" ))
+    tables <- list.files(file.path(path_to_data, "analysis", state, folder, "results", recent_folder, "tables" ), full.names=T)
     
-    mod_file <- tables[grepl("mod_dat.csv" , tables)]
+    mod_files <- tables[grepl("mod_dat.csv" , tables)]
     
-    if (length(mod_file)>1){
-      warning(paste("More than 1 file listed with 'mod_dat.csv'. Using first file. Check folder:",  toString(file.path(path_to_data, "analysis", state, folder, "results", recent_folder, "tables" ))))
-      mod_file <- mod_file[1]
+    
+    if (length(mod_files) == 0){
+      warning(paste("folder contains no `mod_dat.csv` files.", toString(file.path(path_to_data, "analysis", state, folder, "results", recent_folder, "tables" ))))
+    } else { 
+      if (length(mod_files)>1){
+        warning(paste("More than 1 file listed with 'mod_dat.csv'. Combined multiple files. Check folder:",  toString(file.path(path_to_data, "analysis", state, folder, "results", recent_folder, "tables" ))))
+      } 
+      
+      #Load site name, timezone, and other metadata for the site
+      # source(file.path(path_to_data, folder, 'scripts/0_master_file.R'), echo = F)
+      # print(site_tz)
+      # print(site)
+      
+      file_site_list <- strsplit(mod_files, "_")
+      name_length <- unlist(lapply(file_site_list, length))
+      file_site_name <- tail(unlist(strsplit(unlist(file_site_list[which.min(name_length)])[1], "/")),1)
+      
+      # if (identical(site, file_site_name)==FALSE){
+      #   warning(paste("Site name in `0_master_file.R` does not match site name in data_cached folder:", toString(folder)))
+      #   }
+      
+      #load and combine all model (mod) files
+      
+      data_i <- lapply(mod_files, read.csv, stringsAsFactors = F, header=T) %>%
+        bind_rows(.id = "file_id") %>%
+        arrange(unique_storm_number) %>%
+        # select(-file_id) %>%
+        distinct() %>%
+        mutate(storm_start = as.POSIXct(storm_start, tz=state_tz),
+               storm_end = as.POSIXct(storm_end, tz=state_tz),
+               rain_startdate = as.POSIXct(rain_startdate, tz=state_tz),
+               rain_enddate = as.POSIXct(rain_enddate, tz=state_tz),
+               ant_discharge_date = as.POSIXct(ant_discharge_date, tz=state_tz))
+      
+      #place data into state list
+      statesites_list[[folder_nu]]<-data_i
+      # names(statesites_list)[[folder_nu]] <- file_site_name
+      
     }
-  
-    #Load site name, timezone, and other metadata for the site
-    # source(file.path(path_to_data, folder, 'scripts/0_master_file.R'), echo = F)
-    # print(site_tz)
-    # print(site)
-    
-    file_site_name <- paste(unlist(strsplit(mod_file, "_"))[1], unlist(strsplit(mod_file, "_"))[2], sep="_")
-    
-    # if (identical(site, file_site_name)==FALSE){
-    #   warning(paste("Site name in `0_master_file.R` does not match site name in data_cached folder:", toString(folder)))
-    #   }
-    
-    #Identify model (mod) file and load
-    mod_path <- file.path(path_to_data, "analysis", state, folder, "results", recent_folder, "tables", mod_file)
-    data_i <- read.csv(file_in(mod_path), stringsAsFactors = F, header=T)
-    
-    #Change timezone based on `0_master_file` script
-    data_i <- data_i %>%
-      mutate(storm_start = as.POSIXct(storm_start, tz=state_tz),
-             storm_end = as.POSIXct(storm_end, tz=state_tz),
-             rain_startdate = as.POSIXct(rain_startdate, tz=state_tz),
-             rain_enddate = as.POSIXct(rain_enddate, tz=state_tz),
-             ant_discharge_date = as.POSIXct(ant_discharge_date, tz=state_tz))
-    
-    #place data frame into a list
-    statesites_list[[folder_nu]]<-data_i
-    names(statesites_list)[[folder_nu]] <- file_site_name
-    
   }
+  
   allsites_list[[state_nu]] <- ldply(statesites_list, data.frame, .id = "site")
   
 }
@@ -87,6 +97,4 @@ for (state_nu in 1:length(states)){
 data_df <- ldply(allsites_list, data.frame, .id = "state")
 
 
-# head(data_df)
-# str(data_df)
 

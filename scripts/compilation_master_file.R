c61af2dba04cb69ba8f67663dea2606d16ae5b4a
Luke Loken <lukeloken@gmail.com>
2020-01-22 08:50:12 -0600

    Master run file for processing cross site data. Change "path_to_data" to match location of project data

--- scripts/compilation_master_file.R
+++ scripts/compilation_master_file.R
@@ -1,4 +1,7 @@
 
 
-# Work flow for data compilation and cross-site analysis
+# Master file for running all cross-site comparisons
 
+path_to_data <- "M:/NonPoint Evaluation/GLRI Edge-of-field/R-analysis"
+
+source('scripts/run_files/compilation_run_file.R')

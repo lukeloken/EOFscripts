ab8fd95cf42492361278e337a0a8a2d9cbfc393f
Luke Loken <lukeloken@gmail.com>
2020-01-24 13:17:01 -0600

    include manure in soil pca. Plot pca with manure colored

--- scripts/data_compliation/3_load_soil_data.R
+++ scripts/data_compliation/3_load_soil_data.R
@@ -15,12 +15,12 @@ soil_spring <- soil_df %>%
 
 
 
-soilvars <- names(soil_spring[4:ncol(soil_spring)])
+soilvars <- names(soil_spring[3:ncol(soil_spring)])
 
 i=1
 soil_dots_list<- list()
 
-for (i in 1:length(soilvars)){
+for (i in 2:length(soilvars)){
 soil_dots_list[[i]] <- ggplot(data=soil_spring, 
                               aes_string(x="Site", y=soilvars[i], group="Depth")) +
   geom_point(aes(col=Depth)) +
@@ -50,9 +50,15 @@ grid.arrange(plot_grid, mylegend, nrow=2, heights=c(15,1))
 #PCA
 
 pca_df<-soil_spring[,soilvars]
+Manure_temp <- factor(pca_df$Manure, c('Manure', "No Manure"))
+Manure_temp <- as.numeric(Manure_temp)
+Manure_temp[which(Manure_temp == 2)] <- 0
+pca_df$Manure_binary <- Manure_temp
 pca_df<-na.omit(pca_df)
 
-pca <- prcomp(pca_df, center = TRUE, scale. = TRUE, rank=6) 
+pca_df2 <- select(pca_df, -Manure)
+
+pca <- prcomp(pca_df2, center = TRUE, scale. = TRUE, rank=6) 
 
 
 plot(pca, type='l')
@@ -79,8 +85,9 @@ mtext('PCA #',1,2)
 corrange <- c(NA, NA)
 corrange[1] <- floor(min(pca$rotation)*10)/10
 corrange[2] <- ceiling(max(pca$rotation)*10)/10
-corrange[which.max(abs(corrange))] <- corrange +c(-.1, .1)
-
+if (mean(corrange) != 0) {
+  corrange[which.min(abs(corrange))] <- corrange[which.max(abs(corrange))]*(-1)
+}
 corrplot(pca$rotation, is.corr=FALSE, mar=c(0,0,0,1.5), oma=c(0,0,0,0), tl.col='black', cl.pos='r', cl.ratio=0.5, col=brewer.pal(10, 'RdYlBu'), cl.lim=corrange)
 
 mtext('Correlation', 4, -2)
@@ -91,7 +98,7 @@ mtext('Predictor variable', 2, 1.5)
 dev.off()
 
 
-res <- cor(pca_df)
+res <- cor(pca_df2)
 round(res, 2)
 
 png(file_out(file.path(path_to_results, "Figures", "Soil", "SoilCorrelation_Spring2016.png")), res=400, width=7, height=7, units='in')
@@ -112,14 +119,14 @@ par(mfrow=c(1,1))
 par(mar=c(2.5,3.5,.5,0), oma=c(.5,0,0,0))
 par(mgp=c(2, .5, 0))
 
-chart.Correlation(pca_df, histogram=TRUE, pch=19)
+chart.Correlation(pca_df2, histogram=TRUE, pch=19)
 
 dev.off()
 
 
 
 
-pca <- prcomp(pca_df, center = TRUE, scale. = TRUE, rank=5) 
+# pca <- prcomp(pca_df, center = TRUE, scale. = TRUE, rank=5) 
 
 
 #plot axes 1 and 2 of PCA
@@ -129,13 +136,16 @@ par(mar=c(1.5,3,.5,.5), oma=c(2.5,1,0,0))
 par(mgp=c(2, .5, 0))
 
 
-p_1v2<-autoplot(pca, x=2, y=1, data=pca_df, size=3, 
+p_1v2<-autoplot(pca, x=2, y=1, data=pca_df, size=3, colour = "Manure",
                 loadings = TRUE, loadings.colour = 'grey', 
                 loadings.label = TRUE, loadings.label.colour='black', 
                 loadings.label.size = 3) + 
-  scale_color_manual(values=rev(viridis(4))) + 
+  scale_color_manual(values=c('#d95f02', '#1b9e77')) + 
   theme_bw() + 
-  theme(legend.position=c(0.83,0.15), panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+  theme(legend.position=c(0.15,0.12), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        legend.title = element_blank())
 
 print(p_1v2)  
 
@@ -149,11 +159,11 @@ par(mar=c(1.5,3,.5,.5), oma=c(2.5,1,0,0))
 par(mgp=c(2, .5, 0))
 
 
-p_3v2<-autoplot(pca, x=3, y=1, data=pca_df, size=3, 
+p_3v2<-autoplot(pca, x=3, y=1, data=pca_df, size=3, colour = "Manure",
                 loadings = TRUE, loadings.colour = 'grey', 
                 loadings.label = TRUE, loadings.label.colour='black', 
                 loadings.label.size = 3) + 
-  scale_color_manual(values=rev(viridis(4))) + 
+  scale_color_manual(values=c('#d95f02', '#1b9e77')) + 
   theme_bw() + 
   theme(legend.position='none', panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 

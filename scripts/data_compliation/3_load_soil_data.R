2d1b764da1ea5ec40b34ce0c12b8db65820e1299
Luke Loken <lukeloken@gmail.com>
2020-01-24 18:01:10 -0600

    playing with soil data. Now calculate percent silt and sand for 0 to 15 cm

--- scripts/data_compliation/3_load_soil_data.R
+++ scripts/data_compliation/3_load_soil_data.R
@@ -5,14 +5,56 @@ soil_df <- read.csv(file_in(file.path(path_to_data, 'soil', 'raw_data', "EoF_Soi
 
 str(soil_df)
 
+unique(soil_df$Site)
+
+soil_dates<-data.frame(Site = c("WI-01", "WI-02", "WI-03", "WI-04", "WI-05", "OH-01", "MI-01", "MI-02", "IN-01", "IN-02", "NY-01", "NY-02", "NY-03", "NY-04" ), 
+                       Date = as.Date(c("2016-06-21", "2016-06-21", "2016-06-21", "2016-06-22", "2016-06-22", "2016-06-14", "2016-06-14", "2016-06-14", "2016-06-13", "2016-06-13", "2016-06-16", "2016-06-16", "2016-06-16", "2016-06-16" )))
+
 unique(soil_df$Depth)
 
 soil_spring <- soil_df %>%
   dplyr::filter(Type == "Spring") %>%
+  left_join(soil_dates) %>%
+  select(Site, Date, Depth) %>%
+  left_join(soil_df) %>%
   group_by(Site, Depth, Manure) %>%
   summarize_at(vars(P_Clay:NAG_nmol), mean, na.rm=T) %>%
-  select_if(~sum(!is.na(.)) > 0)
+  select_if(~sum(!is.na(.)) > 0) 
 
+
+# soil_test <- soil_df %>%
+#   filter(Site =='IN-01', Depth %in% c("05", "15", "0-15"), Type=='Spring') %>%
+#   select(c(2:4,9:13)) %>%
+#   spread(key = Depth, value = P_Clay)
+
+#Calculate percent silt and sand for 0-15 combined depth
+soil_per <- filter(soil_df, Type=='Spring') %>%
+  select(Site, SamplePt, Depth, P_Clay, P_Silt, P_Sand, Bulk_Den) %>%
+  gather(variable, value, -(Site:Depth)) %>%
+  unite(temp, variable, Depth) %>%
+  spread(temp, value) %>%
+  mutate(`P_Sand_0-15` = CombineDepthPercent(P_Sand_05, Bulk_Den_05, 5, P_Sand_15, Bulk_Den_15, 10),
+         `P_Silt_0-15` = CombineDepthPercent(P_Silt_05, Bulk_Den_05, 5, P_Silt_15, Bulk_Den_15, 10),
+         `Bulk_Den_0-15` = Bulk_Den_05/3 + Bulk_Den_15*2/3) %>%
+  tidyr::gather(variable, value, 3:18) 
+
+#Now need to split (opposite of unite)
+  
+
+soil_per$`P_Sand_0-15` + soil_per$`P_Silt_0-15` + soil_per$`P_Clay_0-15`
+
+soil_0_15 <- filter(soil_df, Depth =="0-15") 
+soil_05 <- filter(soil_df, Depth =="05") 
+soil_15 <- filter(soil_df, Depth =="15") 
+
+soil_0_15$P_Clay
+P_Clay_0_15 
+
+P_Clay_15 <-soil_15$P_Clay
+P_Clay_05 <-soil_05$P_Clay
+
+ggplot(soil_df, aes(x=P_Clay, y=P_Silt)) +
+  geom_point()
 
 
 soilvars <- names(soil_spring[3:ncol(soil_spring)])

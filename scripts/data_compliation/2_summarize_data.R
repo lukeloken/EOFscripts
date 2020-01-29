7daf0422180ea5561ed75931699b1402776bf6b2
Luke Loken <lukeloken@gmail.com>
2020-01-29 11:58:41 -0600

    add rain variables to plot add frozen subset of figures

--- scripts/data_compliation/2_summarize_data.R
+++ scripts/data_compliation/2_summarize_data.R
@@ -35,6 +35,9 @@ concvars <- c('suspended_sediment_conc_mgL',
               'toc_conc_mgL',
               'doc_conc_mgL')
 
+rainvars <-c("rain", "duration", "Ievent", "I5", "I10", "I15", "I30", "I60",
+             "energy_m1", "erosivity_m1", "energy_m2", "erosivity_m2")
+
 
 data_df2 <- data_df %>%
   mutate(site = as.character(site),
@@ -56,6 +59,7 @@ data_df3 <- bind_cols(data_df2, conc_df)
 #Subste to non-frozen conditions
 data_df4 <-filter(data_df3, frozen==FALSE)
 
+data_df5 <-filter(data_df3, frozen==TRUE)
 
 data_wateryear_summary <- data_df2 %>%
   group_by (site, wateryear, period) %>%
@@ -125,6 +129,34 @@ for (var_i in 1:length(concvars)){
 
 
 
+RainByRunnoff_plotlist <- list()
+
+var_i <- 1
+for (var_i in 1:length(rainvars)){
+  
+  RainByRunnoff_plotlist[[var_i]] <- ggplot(data=data_df2, aes_string(x="sum_runoff", y=rainvars[var_i], group="wateryear", color="wateryear", fill="wateryear")) +
+    # scale_x_log10() +
+    # scale_y_log10() +
+    geom_point(aes(shape=frozen), size=2) +
+    scale_shape_manual(values=c(16, 1))+
+    stat_smooth(method = "lm", se=T, alpha=.1) +
+    facet_wrap(~site, scales='free') +
+    theme_bw() +
+    theme(legend.position = 'bottom') +
+    guides(color = guide_legend(nrow = 1)) +
+    labs(x = "Runoff volume (cubic feet per storm)")
+  
+  print(RainByRunnoff_plotlist[[var_i]])
+  
+  ggsave(file_out(file.path(path_to_results, "Figures", "Rain", paste0(rainvars[var_i], "ByRunoff_plot.png"))), RainByRunnoff_plotlist[[var_i]], height=8, width=12, units = 'in', dpi=320)
+  
+}
+
+#############
+# Boxplots
+##############
+
+
 
 ConcByYear_boxlist<-list()
 var_i <- 1
@@ -178,6 +210,81 @@ for (var_i in 1:length(concvars)){
   
 }
 
+
+
+ConcByYear_Frozen_boxlist<-list()
+var_i <- 1
+for (var_i in 1:length(concvars)){
+  
+  ConcByYear_Frozen_boxlist[[var_i]] <- ggplot(data=data_df5, aes_string(x="wateryear", y=concvars[var_i], group="wateryear", color="wateryear", fill="wateryear")) +
+    scale_y_log10() +
+    geom_jitter(width = .1, size=1, alpha=.5, shape=16) + 
+    geom_boxplot(alpha=0.2, outlier.shape = NA) +
+    # scale_shape_manual(values=c(16, 1))+
+    # stat_smooth(method = "lm", se=T, alpha=.1) +
+    facet_wrap(~site, scales='free_y') +
+    theme_bw() +
+    theme(legend.position = 'bottom') +
+    guides(color = guide_legend(nrow = 1)) +
+    labs(x = "Water year") +
+    theme(axis.text=element_text(size=8)) +
+    ggtitle('Frozen runoff events') +
+    theme(plot.title = element_text(hjust = 0.5))
+  
+  print(ConcByYear_Frozen_boxlist[[var_i]])
+  
+  ggsave(file_out(file.path(path_to_results, "Figures", "Boxplots", paste0(concvars[var_i], "ByYear_Frozen_boxplot.png"))), ConcByYear_Frozen_boxlist[[var_i]], height=8, width=12, units = 'in', dpi=320)
+  
+}
+
+
+
+#Rain
+RainByYear_boxlist<-list()
+var_i <- 1
+for (var_i in 1:length(rainvars)){
+    
+  
+  RainByYear_boxlist[[var_i]] <- ggplot(data=data_df3, aes_string(x="wateryear", y=rainvars[var_i], group="wateryear", color="wateryear", fill="wateryear")) +
+    # scale_y_log10() +
+    geom_jitter(width = .1, size=1, alpha=.5, shape=16) + 
+    geom_boxplot(alpha=0.2, outlier.shape = NA) +
+    # scale_shape_manual(values=c(16, 1))+
+    # stat_smooth(method = "lm", se=T, alpha=.1) +
+    facet_wrap(~site, scales='free_y') +
+    theme_bw() +
+    theme(legend.position = 'bottom') +
+    guides(color = guide_legend(nrow = 1)) +
+    labs(x = "Water year") +
+    theme(axis.text=element_text(size=8)) +
+    ggtitle('All runoff events') +
+    theme(plot.title = element_text(hjust = 0.5))
+  
+  print(RainByYear_boxlist[[var_i]])
+  
+  ggsave(file_out(file.path(path_to_results, "Figures", "Rain_Boxplots", paste0(rainvars[var_i], "ByYear_boxplot.png"))), RainByYear_boxlist[[var_i]], height=8, width=12, units = 'in', dpi=320)
+  
+}
+
+
+RunoffByYear_box <- ggplot(data=data_df3, aes_string(x="wateryear", y="sum_runoff", group="wateryear", color="wateryear", fill="wateryear")) +
+  scale_y_log10() +
+  geom_jitter(width = .1, size=1, alpha=.5, shape=16) + 
+  geom_boxplot(alpha=0.2, outlier.shape = NA) +
+  # scale_shape_manual(values=c(16, 1))+
+  # stat_smooth(method = "lm", se=T, alpha=.1) +
+  facet_wrap(~site, scales='free_y') +
+  theme_bw() +
+  theme(legend.position = 'bottom') +
+  guides(color = guide_legend(nrow = 1)) +
+  labs(x = "Water year") +
+  theme(axis.text=element_text(size=8)) +
+  ggtitle('All runoff events') +
+  theme(plot.title = element_text(hjust = 0.5))
+
+print(RunoffByYear_box)
+
+ggsave(file_out(file.path(path_to_results, "Figures", "Rain_Boxplots", paste0("sum_runoff", "ByYear_boxplot.png"))), RunoffByYear_box, height=8, width=12, units = 'in', dpi=320)
 
 
 # ggsave(file_out(file.path(path_to_results, "Figures", "TPLoadByRunoff_plot.png")), TPLoadByRunoff_plot, height=8, width=12, units = 'in', dpi=320)

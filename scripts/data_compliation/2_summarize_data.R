50038bc8bfe7c234c01a9ae22f1c8c9505938ac9
Luke Loken <lukeloken@gmail.com>
2020-01-29 16:24:55 -0600

    add another figure that does not have log yaxis

--- scripts/data_compliation/2_summarize_data.R
+++ scripts/data_compliation/2_summarize_data.R
@@ -128,6 +128,27 @@ for (var_i in 1:length(concvars)){
 }
 
 
+ConcByRunnoff_log_plotlist<-list()
+var_i <- 1
+for (var_i in 1:length(concvars)){
+  
+  ConcByRunnoff_log_plotlist[[var_i]] <- ggplot(data=data_df3, aes_string(x="sum_runoff", y=concvars[var_i], group="wateryear", color="wateryear", fill="wateryear")) +
+    scale_x_log10() +
+    # scale_y_log10() +
+    geom_point(aes(shape=frozen), size=2) +
+    scale_shape_manual(values=c(16, 1))+
+    stat_smooth(method = "lm", se=T, alpha=.1) +
+    facet_wrap(~site, scales='free') +
+    theme_bw() +
+    theme(legend.position = 'bottom') +
+    guides(color = guide_legend(nrow = 1)) +
+    labs(x = "Runoff volume (cubic feet per storm)")
+  
+  print(ConcByRunnoff_log_plotlist[[var_i]])
+  
+  ggsave(file_out(file.path(path_to_results, "Figures", "Conc", paste0(concvars[var_i], "ByRunoff_plot.png"))), ConcByRunnoff_log_plotlist[[var_i]], height=8, width=12, units = 'in', dpi=320)
+  
+}
 
 RainByRunnoff_plotlist <- list()
 

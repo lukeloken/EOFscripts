24714e8177c9ce8defaf953c7028e6c25e0033f9
Luke Loken <lukeloken@gmail.com>
2020-02-06 12:57:35 -0600

    Completed step 2 where it calculates a random forest model pre BMP and calculates differences before/after

--- scripts/run_files/percent_change_run_file.R
+++ scripts/run_files/percent_change_run_file.R
@@ -53,8 +53,9 @@ all_sites <- unique(data_df$site)
 
 # Loop through each site and perform the same analysis
 #Eventually each of these 'steps' will be there own script. 
+
 site_nu <- "WI-SW3"
-for (site_nu in all_sites){
+# for (site_nu in all_sites){
   
   
   #before step 0
@@ -198,61 +199,61 @@ for (site_nu in all_sites){
     
     top.vars <- pdp::topPredictors(mod, n = 4)
     
-    pdf(paste0('figures/', site, '_rf_pp_', responses[i], '.pdf'), heigh = 6, width = 6)
-    par(mfcol = c(2,2), mar = c(4,2,2,2), oma = c(2,2,3,0))
-    
-    for (k in top.vars){
-      
-      partialPlot(mod, pred.data = dat.mod, x.var = mod_env$k,
-                  xlab = k, main = "")
-    }
-    mtext(paste0("Partial Dependence plots - ", clean_names[i]), side = 3, outer = T)
-    dev.off()
+    # pdf(paste0('figures/', site, '_rf_pp_', responses[i], '.pdf'), heigh = 6, width = 6)
+    # par(mfcol = c(2,2), mar = c(4,2,2,2), oma = c(2,2,3,0))
+    # 
+    # for (k in top.vars){
+    #   
+    #   partialPlot(mod, pred.data = dat.mod, x.var = mod_env$k,
+    #               xlab = k, main = "")
+    # }
+    # mtext(paste0("Partial Dependence plots - ", clean_names[i]), side = 3, outer = T)
+    # dev.off()
     
     # change order of levels
     resid.test$period <- factor(resid.test$period, levels = c('before', 'after'))
     ##########
     # now create 4 plots
     # 1-obs vs pred, 2-residual boxplot, 3-residual~fitted, 4-resid~date
-    fig.name = paste0('figures/', site, '_rf_modsum_', responses[i], '.pdf')
-    pdf(fig.name, height = 10, width = 10)
-    layout_matrix <- matrix(c(1:4), nrow=2, ncol=2, byrow=TRUE)
-    layout(layout_matrix)
-    par(mar = c(5,5,3,1), oma = c(0,0,0,0), pch = 16)
-    
-    ####
-    plot(mod$y ~ mod$predicted,
-         xlab = "Fitted Values",
-         ylab = "Observed Values", 
-         main = paste('log10', clean_names[i]), col = as.factor(resid.test$period))
-    
-    abline(0,1)
-    
-    text(x = min(mod$predicted) + 0.2, y = max(mod$y)-0.2, 
-         labels = paste0('% Var Exp = ', round(mod$rsq[500]*100, 1)), 
-         col = 'blue', pos = 4)
-    ####
-    temp <- boxplot(resid.test$resids ~ resid.test$period, 
-                    ylab = 'Residuals', col = c('darkgray', 'red'),
-                    ylim = c(min(resid), max(resid)*1.3), main = test.text)
-    
-    # add mdc after calculation back in for plots? 
-    #text(x = 2, y = max(resid)*1.2, labels = paste0("MDC = ", round(mdc.perc.nafter[i],0), "%"), adj=c(0.5, 0))
-    
-    if (pval < 0.05) {
-      text(x = 2, y = temp$stats[5,2]*1.1, labels = "*", adj=c(0.5, .5), cex = 3)
-    }
-    
-    ###
-    plot(resid ~ mod$predicted, 
-         xlab = "Fitted Values", 
-         ylab = "Residuals", col = as.factor(resid.test$period))
-    abline(h = 0)
-    ## #
-    plot(resid ~ as.Date(dat.mod$storm_start), col = as.factor(resid.test$period),
-         xlab = 'Year', ylab = 'Residuals')
-    abline(h = 0, lwd = 2)
-    dev.off()
+    # fig.name = paste0('figures/', site, '_rf_modsum_', responses[i], '.pdf')
+    # pdf(fig.name, height = 10, width = 10)
+    # layout_matrix <- matrix(c(1:4), nrow=2, ncol=2, byrow=TRUE)
+    # layout(layout_matrix)
+    # par(mar = c(5,5,3,1), oma = c(0,0,0,0), pch = 16)
+    # 
+    # ####
+    # plot(mod$y ~ mod$predicted,
+    #      xlab = "Fitted Values",
+    #      ylab = "Observed Values", 
+    #      main = paste('log10', clean_names[i]), col = as.factor(resid.test$period))
+    # 
+    # abline(0,1)
+    # 
+    # text(x = min(mod$predicted) + 0.2, y = max(mod$y)-0.2, 
+    #      labels = paste0('% Var Exp = ', round(mod$rsq[500]*100, 1)), 
+    #      col = 'blue', pos = 4)
+    # ####
+    # temp <- boxplot(resid.test$resids ~ resid.test$period, 
+    #                 ylab = 'Residuals', col = c('darkgray', 'red'),
+    #                 ylim = c(min(resid), max(resid)*1.3), main = test.text)
+    # 
+    # # add mdc after calculation back in for plots? 
+    # #text(x = 2, y = max(resid)*1.2, labels = paste0("MDC = ", round(mdc.perc.nafter[i],0), "%"), adj=c(0.5, 0))
+    # 
+    # if (pval < 0.05) {
+    #   text(x = 2, y = temp$stats[5,2]*1.1, labels = "*", adj=c(0.5, .5), cex = 3)
+    # }
+    # 
+    # ###
+    # plot(resid ~ mod$predicted, 
+    #      xlab = "Fitted Values", 
+    #      ylab = "Residuals", col = as.factor(resid.test$period))
+    # abline(h = 0)
+    # ## #
+    # plot(resid ~ as.Date(dat.mod$storm_start), col = as.factor(resid.test$period),
+    #      xlab = 'Year', ylab = 'Residuals')
+    # abline(h = 0, lwd = 2)
+    # dev.off()
   }
   
   # create a dataframe describing the residual models
@@ -261,8 +262,8 @@ for (site_nu in all_sites){
                                    pvals = round(pval.differences, 2),
                                    pvals_reduction = round(pval.less, 2),
                                    pvals_increase = round(pval.greater, 2))
-  temp_filename <- file.path('data_cached', paste0(site, '_before_after_residual_analysis.csv'))
-  write.csv(file = temp_filename, x = before_after_resid, row.names = F)
+  # temp_filename <- file.path('data_cached', paste0(site, '_before_after_residual_analysis.csv'))
+  # write.csv(file = temp_filename, x = before_after_resid, row.names = F)
   
   # test if the loop actually calculated values
   
@@ -366,13 +367,13 @@ for (site_nu in all_sites){
                                perc_diff_nonfrozen = round(mean.diff.nonfrozen*100, 1),
                                sd_perc_diff_nonfrozen = round(mean.diff.sd.nonfrozen*100, 1))
   
-  temp_filename <- file.path('data_cached', paste0(site, '_percent_reduction_before_after.csv'))
-  write.csv(perc_reduction, temp_filename, row.names = F)
+  # temp_filename <- file.path('data_cached', paste0(site, '_percent_reduction_before_after.csv'))
+  # write.csv(perc_reduction, temp_filename, row.names = F)
   
   
   
-  
-}
+  #end
+# }
 
 # source the mod.dat prep step that will feed into both MDC calculations and modeling
 source('scripts/data_analysis/0_before_after_datmod_prep.R', echo = F, local = mod_env)

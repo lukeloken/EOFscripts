4a2e32df4c3fe1abe7cd05c3211938e6d7c4b01a
Luke Loken <lukeloken@gmail.com>
2020-02-24 11:13:24 -0600

    changed file name for soil load

--- scripts/run_files/compilation_run_file.R
+++ scripts/run_files/compilation_run_file.R
@@ -11,19 +11,20 @@ source('scripts/functions/not_all_na.R')
 #These data contain estimated loads
 source('scripts/data_compliation/1_load_all_siteapproved_data.R')
 
-
 #load all surface water data
 #output is a data.frame named "data_df"
 source('scripts/data_compliation/1_load_all_data.R')
+
+#load soil data
+# as of January 2020, this only includes spring 2016 and fall 2017 data
+source('scripts/data_compliation/3_load_soil_data.R')
+
 
 
 #summarize water quality data
 #Currently makes lots of figures
 source('scripts/data_compliation/2_summarize_data.R')
 
-#load soil data
-# as of January 2020, this only includes spring 2016 and fall 2017 data
-source('scripts/data_compliation/3_load_soil_data.R')
 
 #plot loads and concentration data by water year and runoff volume
 source('scripts/data_compliation/4_plot_stormrunoff_data.R')

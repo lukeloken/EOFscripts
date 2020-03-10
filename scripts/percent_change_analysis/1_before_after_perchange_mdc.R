5e6bc146237410e7b829940a00524f5cf1a25e34
Luke Loken <lukeloken@gmail.com>
2020-03-10 16:33:57 -0500

    changing to include non-log10 data

--- scripts/percent_change_analysis/1_before_after_perchange_mdc.R
+++ scripts/percent_change_analysis/1_before_after_perchange_mdc.R
@@ -3,11 +3,15 @@
 #Generate random forest of whole dataset
 library(randomForestExplainer)
 
+dat.mod.before.log10 <- filter(dat.mod.log10, period == 'before')
 dat.mod.before <- filter(dat.mod, period == 'before')
 
 # save MDC as output from loop
 mdc.perc.nbefore <- c()
+mdc.perc.nbefore.log10 <- c()
+
 perc.var <- c()
+perc.var.log10 <- c()
 
 
 # loop through responses to create equation and model
@@ -19,6 +23,8 @@ for (i in 1:length(responses)) {
   # create random forest model - before data
   #mod <- randomForest(mod.equation, data = dat.mod, importance = T, na.action = na.omit)
   mod.before <- randomForest(mod.equation, data = dat.mod.before, importance = T, na.action = na.omit, ntree = 1000)
+  mod.before.log10 <- randomForest(mod.equation, data = dat.mod.before.log10, importance = T, na.action = na.omit, ntree = 1000)
+  
   
   #Look at Random Forest Model
   # print(randomForest::varImpPlot(mod.before))
@@ -26,28 +32,36 @@ for (i in 1:length(responses)) {
   # measure_importance(mod.before)
   # explain_forest(mod.before)
   
+  perc.var.log10[i] <- round(mod.before.log10$rsq[1000]*100, 1)
   perc.var[i] <- round(mod.before$rsq[1000]*100, 1)
   
   # calculate minimum detectable change for each constituent based on this model
   
   # pull out mod.before mean squared error
   mse.before <- mod.before$mse[length(mod.before$mse)]
+  mse.before.log10 <- mod.before.log10$mse[length(mod.before.log10$mse)]
+  
   
   n.before <- nrow(dat.mod.before[dat.mod.before$period == "before", ])
-  
+  n.before.log10 <- nrow(dat.mod.before.log10[dat.mod.before.log10$period == "before", ])
   
   
   tval.nbefore <- qt(0.05, n.before + n.before - 2, lower.tail = FALSE)
   
   mdc.nbefore <- tval.nbefore*sqrt((mse.before/n.before) + (mse.before/n.before))
+  mdc.nbefore.log10 <- tval.nbefore*sqrt((mse.before.log10/n.before.log10) + (mse.before.log10/n.before.log10))
   
   mdc.perc.nbefore[i] <- (1-(10^-mdc.nbefore))*100
+  mdc.perc.nbefore.log10[i] <- (1-(10^-mdc.nbefore.log10))*100
   
 }
 # create a dataframe describing the residual models
 mdc <- data.frame(variable = responses,
                   model_fit = perc.var,
-                  mdc = round(mdc.perc.nbefore, 0))
+                  mdc = round(mdc.perc.nbefore, 0),
+                  model_fit.log10 = perc.var.log10,
+                  mdc.log10 = round(mdc.perc.nbefore.log10, 0)
+)
 
 
 if (nrow(mdc) == length(responses)) {

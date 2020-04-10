fcd83ba5cb57075306ee7b2f4e39e9088aeb6aa6
Luke Loken <lukeloken@gmail.com>
2020-04-10 15:41:27 -0500

    including more soil predictors, slope, etc and making glm models

--- scripts/data_compliation/3_combine_soil_water_data.R
+++ scripts/data_compliation/3_combine_soil_water_data.R
@@ -2,19 +2,22 @@
 #load storm data
 data_df3 <- readRDS(file=(file_in(file.path(path_to_data, "compiled_data", "storm_event_loads", "storm_event_conc_allsites_model.rds" ))))
 
-# data_df <- readRDS(file=(file_out(file.path(path_to_data, "compiled_data", "storm_event_loads", "storm_event_loads_allsites_approved_data.rds" ))))
-# 
-# conc_df <- data.frame(sapply(data_df[,loadvars], function (x) x/data_df$runoff_volume*453592/28.3168))
-# colnames(conc_df) <- concvars
-# 
-# #Bind concentration data frame to bigger data frame
-# data_df <- bind_cols(data_df, conc_df)
+#load farm data
+farm_area <- read.csv(file_in(file.path(path_to_data, "SiteCharacteristics", "EOF_WatershedAreas.csv")), header=T, stringsAsFactors = F) %>%
+  select(-Slope)
 
 #Load soil data
-soil_0_15 <- readRDS(file=file_in(file.path(path_to_data, 'soil', 'cleaned_data', 'Soil2016_0_to_15cm.rds')))
+soil_0_15 <- readRDS(file=file_in(file.path(path_to_data, 'soil', 'cleaned_data', 'Soil2016_0_to_15cm.rds'))) %>%
+  select(-Manure) %>%
+  left_join(farm_area) %>%
+  select(-Type)
 
-soil_vars <- names(soil_0_15)[5:34]
+#Variable groups
+soil_vars <- names(soil_0_15)[c(4:25, 26:29,32:37, 39:42)]
 wq_vars <- c(loadvars, concvars, yieldvars, yieldperweqvars)
+
+wq_weightedvars <- wq_vars[which(wq_vars %notin% c('runoff_volume', 'runoff_volume_cubicfootperAcre', 
+                                                   'runoff_cubicmeter_percubicmeterWEQ'))]
 
 #collapse data to only water year with soil data
 #Compute annual metrics
@@ -40,10 +43,8 @@ data_merge_2016 <- data_df3 %>%
   summarize_at(wq_vars, .funs=c(median, mean), na.rm=T) %>%
   left_join(soil_0_15) %>%
   drop_na(Manure) 
-  
+
 #Experimenting with flow weighted means
-wq_weightedvars <- wq_vars[which(wq_vars %notin% c('runoff_volume', 'runoff_volume_cubicfootperAcre', 
-                                                   'runoff_cubicmeter_percubicmeterWEQ'))]
 data_merge2 <- data_df3 %>%
   mutate(wateryear = as.numeric(as.character(wateryear))) %>%
   filter(wateryear %in% c(2016:2017), frozen == 0) %>%
@@ -56,13 +57,14 @@ data_merge2 <- data_df3 %>%
   left_join(soil_0_15) %>%
   drop_na(Manure) %>%
   left_join(data_merge_median[c('Site', 'runoff_volume', 'runoff_volume_cubicfootperAcre', 
-                                'runoff_cubicmeter_percubicmeterWEQ')])
+                                'runoff_cubicmeter_percubicmeterWEQ')]) %>%
+  mutate_if(is.character, as.factor)
 
-  
+
 data_merge <- data_merge2
 
 # ggplot(data=data_merge_2016_2017, aes(x=Bray_P, y=orthophosphate_yield_poundperAcreperInchWEQ)) +
-  # geom_point(aes(color=Manure))
+# geom_point(aes(color=Manure))
 # 
 # ggplot(data=data_merge, aes(x=OM_spring, y=orthophosphate_conc_mgL)) +
 #   geom_point(aes(color=Manure))
@@ -91,14 +93,18 @@ data_merge <- data_merge2
 # choice_yields <-  paste0(yieldvars, "perInchWEQ")[c(1,3,7,8)]
 choice_yields <-  yieldperweqvars[c(1:9,12)]
 choice_conc <- concvars[1:9]
+choice_physics <- c("suspended_sediment_conc_mgL", "suspended_sediment_yield_poundperAcreperInchWEQ", 
+                    "runoff_cubicmeter_percubicmeterWEQ")
 
-choice_soil <-  soil_vars[c(1:3,4:6,7:18, 19:23, 25:30)]
+#If need to subset soil vars
+choice_soil <-  soil_vars[]
 
 predictors.cor <- cor(data_merge[,choice_soil], use = 'complete.obs') 
 names.cor <- row.names(predictors.cor)
 drop.predictors <- caret::findCorrelation(predictors.cor, cutoff = 0.95, verbose = FALSE, exact = TRUE)
 predictors.keep <- c(names.cor[-drop.predictors], 'Manure')
 
+predictors.keep.physics <- soil_vars[c(19:24, 27:36)]
 
 wq_var<-1
 plot_list_yield <-list()
@@ -106,7 +112,7 @@ glm_list_yield <- list()
 model_fig_yield <- list()
 model_data_yield <- list()
 for (wq_var in 1:length(choice_yields)) {
-
+  
   var_list <- list()
   soil_var <- 1
   for (soil_var in 1:length(choice_soil)){
@@ -114,67 +120,86 @@ for (wq_var in 1:length(choice_yields)) {
     lm_model <- lm(data.frame(data_merge[choice_yields[wq_var]])[,1] ~ data.frame(data_merge[choice_soil[soil_var]])[,1])
     pvalue <- summary(lm_model)$coefficients[2,4]
     
-    var_list[[soil_var]] <- ggplot(data=data_merge, aes_string(x=choice_soil[soil_var], y=choice_yields[wq_var])) +
-      geom_point(aes(color=Manure), size=2, shape=1)  +
-      theme_bw() +
-      theme(legend.position = 'none',
-            axis.title.y = element_blank())
-    
-    if (choice_soil[soil_var] %in% predictors.keep) {
-      var_list[[soil_var]] <- var_list[[soil_var]] +
-        geom_point(aes(color=Manure), size=2, shape=16)
+    if (is.numeric(data_merge[[choice_soil[soil_var]]])==FALSE){
+      var_list[[soil_var]] <- ggplot(data=data_merge, aes_string(x=choice_soil[soil_var], y=choice_yields[wq_var])) +
+        geom_point(aes(color=Manure), position=position_jitter(width = 0.1), size=2, shape=16)  +
+        scale_color_brewer(palette='Dark2') + 
+        theme_bw() +
+        theme(legend.position = 'none',
+              axis.title.y = element_blank())
+    } else if (is.numeric(data_merge[[choice_soil[soil_var]]])){
+      
+      var_list[[soil_var]] <- ggplot(data=data_merge, aes_string(x=choice_soil[soil_var], y=choice_yields[wq_var])) +
+        geom_point(aes(color=Manure), size=2, shape=16)  +
+        scale_color_brewer(palette='Dark2') + 
+        theme_bw() +
+        theme(legend.position = 'none',
+              axis.title.y = element_blank())
+      
+      if (choice_soil[soil_var] %in% predictors.keep) {
+        var_list[[soil_var]] <- var_list[[soil_var]] +
+          geom_point(aes(color=Manure), size=2, shape=16)
+      }
+      
+      if (pvalue <=0.1 & pvalue > 0.05) {
+        var_list[[soil_var]] <- var_list[[soil_var]] +
+          geom_smooth(method="lm", se=F, col='darkgrey', linetype='dashed') +
+          geom_point(aes(color=Manure), size=2) 
+      } else if (pvalue <=0.05){
+        var_list[[soil_var]] <- var_list[[soil_var]] +
+          geom_smooth(method="lm", se=F, col='darkgrey') +
+          geom_point(aes(color=Manure), size=2) 
+      }
     }
     
-    if (pvalue <=0.1 & pvalue > 0.05) {
-      var_list[[soil_var]] <- var_list[[soil_var]] +
-        geom_smooth(method="lm", se=F, col='darkgrey', linetype='dashed') +
-        geom_point(aes(color=Manure), size=2) 
-    } else if (pvalue <=0.05){
-      var_list[[soil_var]] <- var_list[[soil_var]] +
-        geom_smooth(method="lm", se=F, col='darkgrey') +
-        geom_point(aes(color=Manure), size=2) 
-    }
     
   }
-   
-
+  
+  
   plot_withLegend <- var_list[[1]] + theme(legend.position = 'bottom', legend.title = element_blank()) + 
     guides(color=guide_legend(ncol=1))
-
+  
   var_list[[length(choice_soil)+1]] <- g_legend(plot_withLegend)
   
   plot_list_yield[[wq_var]] <- grid.arrange(grobs=var_list, ncol=5, top=choice_yields[wq_var])
   
   ggsave(file=file_out(file.path(path_to_results, 'Figures', 'Soil', 'SoilWQ_Scatter', paste(choice_yields[wq_var], ".png"))) , plot_list_yield[[wq_var]], height=8, width=8)
-    
-
+  
+  
   # data.glm <- data_merge[c(choice_soil, choice_yields[wq_var])] %>%
   #   select_if(~ !any(is.na(.))) %>%
   #   data.frame()
   
-  data.glm <- data_merge[c(predictors.keep, choice_yields[wq_var])] %>%
+  data.glm <- data_merge[c(soil_vars[which(soil_vars %notin% c('Texture', 'Soil_Sub_Order'))], choice_yields[wq_var])] %>%
     mutate(Manure = factor(Manure, levels=c('No Manure', "Manure"))) %>%
     select_if(~ !any(is.na(.))) %>%
     data.frame()
-
+  
+  if (choice_yields[wq_var] %in% choice_physics){
+    data.glm <- data_merge[c(predictors.keep.physics, choice_yields[wq_var])] %>%
+      # mutate(Manure = factor(Manure, levels=c('No Manure', "Manure"))) %>%
+      select_if(~ !any(is.na(.))) %>%
+      data.frame()
+  }
+  
   glm_list_yield[[wq_var]]<-bestglm(data.glm, family=gaussian, IC='BIC', nvmax=3)
   print(choice_yields[wq_var])
   glm_list_yield[[wq_var]]
   summary(glm_list_yield[[wq_var]])
-
+  
   model_data_yield[[wq_var]] <- data.frame(obs =glm_list_yield[[wq_var]]$BestModel$model$y,
-                           pred=glm_list_yield[[wq_var]]$BestModel$fitted.values)
-
+                                           pred=glm_list_yield[[wq_var]]$BestModel$fitted.values)
+  
   model_fig_yield[[wq_var]] <- ggplot(data=model_data_yield[[wq_var]], aes(x=obs,
-                                                                   y=pred)) + 
+                                                                           y=pred)) + 
     geom_point() +  
     ggtitle(choice_yields[wq_var]) + 
     labs(x = 'observations', y='predictions' ) + 
     geom_abline() + 
     theme_bw()
-
+  
   print(model_fig_yield[[wq_var]])
-    
+  
 }
 
 
@@ -197,27 +222,39 @@ for (wq_var in 1:length(choice_conc)) {
     lm_model <- lm(data.frame(data_merge[choice_conc[wq_var]])[,1] ~ data.frame(data_merge[choice_soil[soil_var]])[,1])
     pvalue <- summary(lm_model)$coefficients[2,4]
     
-    var_list[[soil_var]] <- ggplot(data=data_merge, aes_string(x=choice_soil[soil_var], y=choice_conc[wq_var])) +
-      geom_point(aes(color=Manure), size=2)  +
-      theme_bw() +
-      theme(legend.position = 'none',
-            axis.title.y = element_blank())
-    
-    if (pvalue <=0.1 & pvalue > 0.05) {
-      var_list[[soil_var]] <- var_list[[soil_var]] +
-        geom_smooth(method="lm", se=F, col='darkgrey', linetype='dashed') +
-        geom_point(aes(color=Manure), size=2) 
-    } else if (pvalue <=0.05){
-      var_list[[soil_var]] <- var_list[[soil_var]] +
-        geom_smooth(method="lm", se=F, col='darkgrey') +
-        geom_point(aes(color=Manure), size=2) 
+    if (is.numeric(data_merge[[choice_soil[soil_var]]])==FALSE){
+      var_list[[soil_var]] <- ggplot(data=data_merge, aes_string(x=choice_soil[soil_var], y=choice_conc[wq_var])) +
+        geom_point(aes(color=Manure), position=position_jitter(width = 0.1), size=2, shape=16)  +
+        scale_color_brewer(palette='Dark2') + 
+        theme_bw() +
+        theme(legend.position = 'none',
+              axis.title.y = element_blank())
+      
+    } else if (is.numeric(data_merge[[choice_soil[soil_var]]])){
+      
+      var_list[[soil_var]] <- ggplot(data=data_merge, aes_string(x=choice_soil[soil_var], y=choice_conc[wq_var])) +
+        geom_point(aes(color=Manure), size=2, shape=16)  +
+        scale_color_brewer(palette='Dark2') + 
+        theme_bw() +
+        theme(legend.position = 'none',
+              axis.title.y = element_blank())
+      
+      if (pvalue <=0.1 & pvalue > 0.05) {
+        var_list[[soil_var]] <- var_list[[soil_var]] +
+          geom_smooth(method="lm", se=F, col='darkgrey', linetype='dashed') +
+          geom_point(aes(color=Manure), size=2) 
+      } else if (pvalue <=0.05){
+        var_list[[soil_var]] <- var_list[[soil_var]] +
+          geom_smooth(method="lm", se=F, col='darkgrey') +
+          geom_point(aes(color=Manure), size=2) 
+      }
     }
     
   }
   
   
-  plot_withLegend <- var_list[[1]] + theme(legend.position = 'bottom', legend.title = element_blank()) + 
-    guides(color=guide_legend(ncol=1))
+  plot_withLegend <- var_list[[1]] + theme(legend.position = 'bottom') + 
+    guides(color=guide_legend(ncol=1, title.position="top"))
   
   var_list[[length(choice_soil)+1]] <- g_legend(plot_withLegend)
   
@@ -226,9 +263,16 @@ for (wq_var in 1:length(choice_conc)) {
   ggsave(file=file_out(file.path(path_to_results, 'Figures', 'Soil', 'SoilWQ_Scatter', paste(choice_conc[wq_var], ".png"))) , plot_list_conc[[wq_var]], height=8, width=8)
   
   
-  data.glm <- data_merge[c(choice_soil, choice_conc[wq_var])] %>%
+  data.glm <- data_merge[c(soil_vars[which(soil_vars %notin% c('Texture', 'Soil_Sub_Order'))], choice_conc[wq_var])] %>%
     select_if(~ !any(is.na(.))) %>%
     data.frame()
+  
+  if (choice_conc[wq_var] %in% choice_physics){
+    data.glm <- data_merge[c(predictors.keep.physics, choice_yields[wq_var])] %>%
+      # mutate(Manure = factor(Manure, levels=c('No Manure', "Manure"))) %>%
+      select_if(~ !any(is.na(.))) %>%
+      data.frame()
+  }
   
   glm_list_conc[[wq_var]]<-bestglm(data.glm, family=gaussian, IC='BIC', nvmax=3)
   print(choice_conc[wq_var])
@@ -237,10 +281,10 @@ for (wq_var in 1:length(choice_conc)) {
   
   
   model_data_conc[[wq_var]] <- data.frame(obs =glm_list_conc[[wq_var]]$BestModel$model$y,
-                                           pred=glm_list_conc[[wq_var]]$BestModel$fitted.values)
+                                          pred=glm_list_conc[[wq_var]]$BestModel$fitted.values)
   
   model_fig_conc[[wq_var]] <- ggplot(data=model_data_conc[[wq_var]], aes(x=obs,
-                                                                           y=pred)) + 
+                                                                         y=pred)) + 
     geom_point() +  
     ggtitle(choice_conc[wq_var]) + 
     labs(x = 'observations', y='predictions' ) + 
@@ -261,10 +305,10 @@ wq_var <-1
 fig_list <- list()
 for (wq_var in 1:length(choice_conc)) {
   
-fig_list[[wq_var]] <- ggplot(data=data_merge, aes_string(x=choice_yields[wq_var], y=choice_conc[wq_var])) +
-  geom_point(aes(color=Manure), size=2)  +
-  theme_bw() +
-  theme(legend.position = 'none')
+  fig_list[[wq_var]] <- ggplot(data=data_merge, aes_string(x=choice_yields[wq_var], y=choice_conc[wq_var])) +
+    geom_point(aes(color=Manure), size=2)  +
+    theme_bw() +
+    theme(legend.position = 'none')
 }
 
 

4b3dd7391b3cbabcfbebdbb05a4651a211cfbdc6
Luke Loken <lukeloken@gmail.com>
2020-10-16 11:05:51 -0500

    Adding GLRI sites to rain event calculation. Saving outputs with all data and no-rain for QAQC

--- scripts/data_publication/CalculateRainEvents.R
+++ scripts/data_publication/CalculateRainEvents.R
@@ -16,21 +16,25 @@ source('scripts/functions/fxns_data_processing.R')
 ieHr <- 2
 # Amount it must rain to count as an event in tenths of inches (rain threshold) -- note RMevents_sample does not use rainthresh
 rainthresh <- 0.008
-# Antecedent Rainfall in days (ARF.days)
-antecedentDays = c(0.5, 1, 7, 14)
 
 
-#unit rain data for all non-GLRI sites
-Rain.uv.combined <- readRDS(file.path(path_to_data, 'compiled_data', 'rain', 'Compiled_Rain_UV_DataPublication.rds'))
 
+#unit rain data for all sites
+Rain.uv.combined.other <- readRDS(file.path(path_to_data, 'compiled_data', 'rain', 'Compiled_Rain_UV_DataPublication.rds'))
 
-final_df <- readRDS(file.path(path_to_data, "Data Publication", 
+Rain.uv.combined.GLRI <- readRDS(file.path(path_to_data, 'compiled_data', 'rain', 'Compiled_Rain_UV_Data.rds'))
+
+Rain.uv.combined <- bind_rows(Rain.uv.combined.other, Rain.uv.combined.GLRI)
+
+combined_df <- readRDS(file.path(path_to_data, "Data Publication", 
                             "CleanedFinalVersions", 
-                            "All_EOF_StormEventLoadsFormatted.rds"))
+                            "All_EOF_StormEventLoadsCombined.rds")) 
 
-final_df$storm_end[which(final_df$FieldName == "JF1" & 
-                           final_df$unique_storm_number == "2015-1")] <- 
+combined_df$storm_end[which(combined_df$FieldName == "JF1" & 
+                              combined_df$unique_storm_number == "2015-1")] <- 
   as.POSIXct("2015-03-08  03:00:00", tz = "America/Chicago")
+
+combined_df <- arrange(combined_df, project, FieldName, site, storm_start)
 
 site_i <- 1
 wq.precip.list <- list()
@@ -38,7 +42,7 @@ for (site_i in 1:nrow(site_table)){
   USGSSiteNumber_i <-  site_table$USGSSiteNumber[site_i]
   RainNumber_i <-  site_table$USGSSiteNumberforPrecipitation[site_i]
   
-  wq.dat <- filter(final_df, site == USGSSiteNumber_i)
+  wq.dat <- as.data.frame(filter(combined_df, site == USGSSiteNumber_i))
 
   precip_raw <- filter(Rain.uv.combined, rain_site == RainNumber_i)
   attributes(precip_raw$pdate)$tzone <- attributes(wq.dat$storm_end)$tzone 
@@ -49,10 +53,17 @@ for (site_i in 1:nrow(site_table)){
     next
   }
 
-  precip.dat <- run.rainmaker(precip_raw = precip_raw, ieHr = 2, rainthresh = 0.008, wq.dat = wq.dat,
+  if(length(which(!is.finite(c(wq.dat$storm_start, wq.dat$storm_end))))>0){
+    warning(paste0("Some observations have NA storm_start or storm_end times. Check ", toString(USGSSiteNumber_i)))
+    wq.dat <- filter(wq.dat, is.finite(storm_start) & is.finite(storm_end))
+  }
+  
+  precip.dat <- run.rainmaker(precip_raw = precip_raw, ieHr = ieHr, 
+                              rainthresh = rainthresh, wq.dat = wq.dat,
                               xmin = c(5,10,15,30,60), antecedentDays = c(1,2,7,14))
   
-  precip.dat <- rename(precip.dat, 'rain_startdate' = 'StartDate', 'rain_enddate' = 'EndDate')
+  precip.dat <- rename(precip.dat, 'rain_startdate' = 'StartDate', 'rain_enddate' = 'EndDate') %>%
+    select(-unique_storm_number)
   
   precip.dat[is.na(precip.dat$rain), which(names(precip.dat) == "duration"):
                which(names(precip.dat) == "ARFdays14")] <- NA
@@ -63,7 +74,7 @@ for (site_i in 1:nrow(site_table)){
   
   wq.precip.list[[site_i]] <- wq.dat %>%
     mutate(USGSSiteNumberforPrecipitation = RainNumber_i) %>%
-    select(-unique_storm_number) %>%
+    # select(-unique_storm_number) %>%
     bind_cols(precip.dat)
   
   print(site_table[site_i,1:3])  
@@ -74,21 +85,46 @@ wq.precip.df <- wq.precip.list %>%
   bind_rows(.id = NULL) %>%
   distinct()
 
+saveRDS(wq.precip.df, file.path(path_to_data, "Data Publication", 
+                               "CleanedFinalVersions", 
+                               "All_EOF_StormEventLoadsRainCalculated.rds"))
+
+
+# View(filter(wq.precip.df, FieldName == 'WI-SW1') %>% select(unique_storm_number, storm_start, storm_end, rain_startdate, rain))
+
+
 rain_summary <- wq.precip.df %>%
   # filter(is.na(rain) | rain == 0) %>%
-  group_by(site, FieldName, project) %>%
+  group_by(project, FieldName, site) %>%
   summarize(n_rain_na = length(which(is.na(rain))),
-            n_rain_0  = length(which(rain == 0)),
-            n_rain_positive = length(which(rain>0)),
+            # n_rain_0  = length(which(rain == 0)),
+            n_rain_0_unfrozen = length(which(rain == 0 & frozen == 0)),
+            n_rain_0_frozen = length(which(rain == 0 & frozen == 1)),
+            n_rain_lessthan0.2 = length(which(rain > 0 & rain<0.2)),
+            n_rain_greaterthan0.2 = length(which(rain >= 0.2)),
             n_total = n())
 
 data.frame(rain_summary)
+colSums(rain_summary[,4:9])
 
 
-runoff_summary <- final_df %>%
-  # filter(is.na(rain) | rain == 0) %>%
-  group_by(site, FieldName, project, frozen) %>%
+runoff_summary <- combined_df %>%
+  mutate(wateryear = getWY(combined_df$storm_start)) %>%
+  group_by(site, FieldName, project, wateryear) %>%
   summarize(n_total = n())
 
 
-data.frame(runoff_summary)
+# data.frame(runoff_summary)
+
+wq.precip.norain <- wq.precip.df %>%
+  filter(is.na(rain) | rain < 0.2) %>%
+  # rename(USGSSiteNumber = site) %>%
+  left_join(select(site_table, USGSSiteNumber, Area), by = c("site" = "USGSSiteNumber")) %>%
+  mutate(USGSSiteNumberforPrecipitation = paste0("'", USGSSiteNumberforPrecipitation),
+         site = paste0("'", site)) %>%
+  mutate(runoff_index = runoff_volume / (rain * Area * 43560 / 12)) %>%
+  arrange(project, FieldName, site, storm_start)
+
+write.csv(wq.precip.norain, file.path(path_to_data, "Data Publication", "All.EOF.missingrain.csv"), row.names = FALSE)
+
+table(wq.precip.norain$FieldName)

e8167bf9f76b4cbbc2b8993a221ab8719db91851
Luke Loken <lukeloken@gmail.com>
2020-10-16 11:07:36 -0500

    using new function to calculate event rain events

--- scripts/functions/fxns_data_processing.R
+++ scripts/functions/fxns_data_processing.R
@@ -6,7 +6,9 @@ run.rainmaker <- function(precip_raw, wq.dat, ieHr = 2, rainthresh = 0.008,
                           xmin = c(5,10,15,30,60), antecedentDays = c(1,3,7,14)) {
   
   # calculate events
-  events <- RMevents_sample(df = precip_raw, ieHr = ieHr, rain = 'rain', time = 'pdate', 
+  # events <- RMevents_sample(df = precip_raw, ieHr = ieHr, rain = 'rain', time = 'pdate', 
+  #                           dfsamples = wq.dat, bdate = 'storm_start', edate = 'storm_end')
+  events <- RMevents_sample_LCL(df = precip_raw, ieHr = ieHr, rain = 'rain', time = 'pdate', 
                             dfsamples = wq.dat, bdate = 'storm_start', edate = 'storm_end')
   
   
